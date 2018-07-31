32d667e2c63dd8e0859f56e1484667033fd0d116
Chad Chaney <cchane4@gmail.com>
2018-07-31 14:47:12 -0500

    error handling, defaul loggging with morgan and automatic server reloading with nodemon

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express'); 
 const app = express(); 
+const morgan = require('morgan'); 
 
 const productRoutes = require('./api/routes/products'); 
 const orderRoutes = require('./api/routes/orders'); 
+// used to log messages through morgan middleware
+app.use(morgan('dev')); 
 
+// Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//no routes in the made routes can handle functions so , output an error
+app.use(( req, res, next) => { 
+    const error = new Error( 'not found'); 
+    error.status = 404; 
+    next(error); 
+})
+
+app.use((error, req, res, next) => { 
+    res.status(error.status || 500); 
+    res.json({ 
+        error:{
+            message: error.message
+        }
+    });
+});
 
 module.exports = app; 
