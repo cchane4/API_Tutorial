40ee08bdba9cf108f601fbb8c627473ab602b260
Chad Chaney <cchane4@gmail.com>
2018-08-10 22:29:47 -0500

    completed building models logic for orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -72,6 +72,11 @@ router.post('/', (req, res, next) => {
 
 router.get('/:orderId', (req, res, next) => { 
     Order.findById(req.params.orderId).exec().then(order => { 
+        if (!order) { 
+            return res.status(404).json({ 
+                message: 'Order not found'
+            });
+        }
         res.status(200).json({ 
             order: order, 
             request: { 
@@ -88,10 +93,23 @@ router.get('/:orderId', (req, res, next) => {
 }); 
 
 router.delete('/:orderId', (req, res, next) => { 
-    res.status(200).json({ 
-        message: 'order deleted',
-        orderId: req.params.orderId
-    }); 
+    Order.remove({ _id: req.params.orderId })
+    .exec()
+    .then(result => { 
+        res.status(200).json({ 
+            message: 'Order deleted', 
+            request: { 
+                type: "POST",
+                url: "http://localhost:3000/orders",
+                body: { productId: 'ID', quantity: 'Number' } 
+            } 
+        }); 
+})
+.catch(err => { 
+            res.status(500).json({ 
+            error: err
+            }); 
+        });
 }); 
 
 module.exports = router; 
